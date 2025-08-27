334d09bd11da653a4b9439fca755c2e79831106f
Cristian E. Guzman Cogolllo <cogolloguzmanesteban@gmail.com>
2025-08-26 20:12:33 -0500

    Agregar funcionalidad para registrar y mostrar productos en la bodega

--- Main.py
+++ Main.py
@@ -1,1 +1,38 @@
 #Ejercicio de la tienda
+Opcion_del_menu=150
+print("---Bodega Online------>")
+print("***********************")
+while Opcion_del_menu != 0:
+    print("1.Registrar un Producto a la APP")
+    print("2.Mostrar productos en Bodega")
+    print("3.Valorar bodega")
+    print("4.Buscar un Producto")
+    print("5.Eliminar un Producto")
+    print("Digita 0 Para salir")
+    Opcion_del_menu=int(input("Digita Una Opcion"))
+
+    #Zona de condiciones
+    if Opcion_del_menu==0:
+        print("Saliendo de la App...")
+        break
+    elif Opcion_del_menu==1:
+        #Debo crear un diccionario desde 0
+        #para almacenar la info de un producto
+        producto={}
+        print("A Continuacion ingresa un Producto a nuestra APP")
+        producto["id"]==input("Digita el ID del producto a registrar")
+        producto["nombre"]==input("Ingesa el nombre del Producto")
+        producto["descripcion"]==input("Cuentanos acerca del Producto")
+        producto["fotografia"]==input("Ingresa la URL de la fotografia del Producto")
+        producto["precio_unitario"]==float(input("digita el precio unitario del producto"))
+        producto["cantidad_en_bodega"]==int(input("Ingresa la cantidad de los productos en bodega"))    
+    elif Opcion_del_menu==2:
+        pass
+    elif Opcion_del_menu==3:
+        pass
+    elif Opcion_del_menu==4:
+        pass
+    elif Opcion_del_menu==5:
+        pass
+    else:
+        print("Opcion Incorrecta, ingresa una opcion valida")
